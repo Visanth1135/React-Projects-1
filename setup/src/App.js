b049623c70429e28323b09629aacad010e48d983
Visanth1135 <k.visanth1996@gmail.com>
2022-05-29 15:35:20 +0400

    Example of Navbar and Modal with useContext

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
-import {Button} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-export default function App() {
-
-
+import React from 'react';
+import Modal from './Modal';
+import Sidebar from './Sidebar';
+import Home from './Home';
+import './style.css'
+function App() {
   return (
-  <div className="col-sm-4 d-grid gap-2">
-  <button type="button" class="btn btn-primary">Left</button>
-  <button type="button" class="btn btn-primary">Middle</button>
-  <button type="button" class="btn btn-primary">Right</button>
-  </div>
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
+}
 
-  )
-  }
+export default App;
