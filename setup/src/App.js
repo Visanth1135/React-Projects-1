cd2e8ac0ba0ce175b65e701fe4ac941e1e2c882a
Visanth1135 <k.visanth1996@gmail.com>
2022-05-29 13:50:25 +0400

    Use cars of useref creating navbar

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,17 +1,12 @@
-import React, { useState } from "react";
-import {Button} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-export default function App() {
-
-
+import React from 'react';
+import Navbar from './Navbar';
+import './style.css'
+function App() {
   return (
-  <div className="col-sm-4 d-grid gap-2">
-  <button type="button" class="btn btn-primary">Left</button>
-  <button type="button" class="btn btn-primary">Middle</button>
-  <button type="button" class="btn btn-primary">Right</button>
-  </div>
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+}
 
-  )
-  }
+export default App;
